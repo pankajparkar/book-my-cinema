6207478fe3f1ed9bbe8dbe957a7bc37cc4d8c778
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-05-20 08:51:21 +0800

    Removed todo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,9 @@ import { NavLink } from './models/navlink.model';
 })
 export class AppComponent {
 
-  // TODO: uncomment for movie app
-  // title = 'Angular-Cinema';
-  // navLinks: NavLink[] = [
-  //   { link: '/', name: 'Home' },
-  //   { link: '/movies', name: 'Movies' },
-  // ]
-
-  title = 'Ticket Admin';
-
+  title = 'Angular-Cinema';
   navLinks: NavLink[] = [
-    { link: '/', name: 'Dashboard' },
-    { link: '/collection', name: '$ Collection' },
-    { link: '/manage-showtimes', name: 'Manage Showtimes' },
-  ]
+    { link: '/', name: 'Home' },
+    { link: '/movies', name: 'Movies' },
+  ];
 }
